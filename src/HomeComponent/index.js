d95f406531bf04d15ef83193e1de92ecb34e8663
huangyg <982044780@qq.com>
2019-01-02 17:45:04 +0800

    test eslint

--- src/HomeComponent/index.js
+++ src/HomeComponent/index.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './index.css';
 
 class App extends Component {
+  
+  componentWillMount() {
+    let a = true;
+    if(a){
+      console.log('test something');
+    }
+  }
+  
   render() {
     return (
       <div className="App">
